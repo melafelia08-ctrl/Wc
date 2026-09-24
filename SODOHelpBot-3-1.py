970e0695c12826dd79d31b65dee9a29131b86fa5
melafelia08-ctrl <melafelia08@gmail.com>
2026-09-24 19:01:47 +0530

    Refactor BOT_TOKEN retrieval and fix variable name
    Updated BOT_TOKEN retrieval to use an empty default value. Fixed variable name in file opening for known users.

--- SODOHelpBot-3-1.py
+++ SODOHelpBot-3-1.py
@@ -12,7 +12,7 @@ from telegram.ext import (
     filters,
 )
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "8609127164"))
 
 BRAND = "🛟 <b>SODO Support</b>"
@@ -23,7 +23,7 @@ USERS_FILE = "known_users.json"
 def load_known_users() -> set:
     if os.path.exists(USERS_FILE):
         try:
-            with open(USERS_FILE, "r") as f:
+            with open(USERS_FILE, "r") as8976765441:AAFlv2Lsph08PcoXCwcx3_78jyqVp1Kw_pg f:
                 return set(json.load(f))
         except (json.JSONDecodeError, IOError):
             return set()
